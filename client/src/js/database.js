f4088049f9445a817882e9fd977943a8ec29c0f9
Sherwin Abraham <sherwinabraham@gmail.com>
2023-10-02 20:24:50 -0500

    Testing the JATE Editor.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -44,8 +44,8 @@ export const getDb = async () => {
   // Open up the desired object store.
   const store = tx.objectStore('jate');
 
-  // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
+  // Use the .get() method to get all data in the database.
+  const request = store.get(1);
 
   // Get confirmation of the request.
   const result = await request;
